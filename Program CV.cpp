aaaefcdc62d92e05a8432d342a00e34b66e28c2b
KepoDes <86428587+Kepoo311@users.noreply.github.com>
2023-05-29 15:36:07 +0000

    Add files via upload

---
+++ Program CV.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <conio.h>
+#include <bitset>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int desimal;
+
+	cout <<"Masukkan Nilai Decimal : ";
+	cin >> desimal;
+	bitset<8> binary(desimal);
+
+    system("cls");
+
+    cout <<"Nilai Decimal : " << desimal << endl;
+    cout <<"Nilai Binary : " << binary.to_string() << endl;
+    cout <<"Nilai Hexadecimal : " << hex << desimal << endl;
+    cout <<"Nilai Octal : " << oct << desimal << endl;
+
+    cout<<"By M.Riandra.Rai.K";
+
+    getch();
+	return 0;
+}
